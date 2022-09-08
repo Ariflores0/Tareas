b4e911b8a1aee53251a004ccad5fc9f951029ab5
Ariflores0 <105399973+Ariflores0@users.noreply.github.com>
2022-09-08 02:38:45 -0500

    Create index2.js

---
+++ index2.js
@@ -0,0 +1,11 @@
+// funcion de Tarea Realizada 
+
+function tareaRealizada(element) {
+    element.classList.toggle(check)
+    element.classList.toggle(uncheck)
+    element.parentNode.querySelector('.text').classList.toggle(lineThrough)
+    LIST[element.id].realizado = LIST[element.id].realizado ?false :true //Si
+   // console.log(LIST)
+   // console.log(LIST[element.id])
+   // console.log(LIST[element.id].realizado)
+}
